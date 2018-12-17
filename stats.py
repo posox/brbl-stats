2c466434334536d24dcdb1b2bb349b5a56105414
Sergey Reshetnyak <sreshetniak@mirantis.com>
2018-12-18 02:07:09 +0400

    Stupid caching based on postgresql

--- stats.py
+++ stats.py
@@ -2,16 +2,54 @@
 
 import json
 import logging
+import os
 
-import cachetools
 import flask
 from flask import Flask
 from instaparser import agents
 from instaparser import entities
+import psycopg2
+import sqlalchemy as sqla
+from sqlalchemy.ext import declarative
+from sqlalchemy.orm import sessionmaker
 
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 log = logging.getLogger(__name__)
 app = Flask(__name__)
+
+agent = agents.Agent()
+
+engine = sqla.create_engine(DATABASE_URL, echo=True)
+Base = declarative.declarative_base()
+Session = sessionmaker(bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    name = sqla.Column(sqla.String, primary_key=True)
+    followers = sqla.Column(sqla.Integer)
+    posts = sqla.Column(sqla.Integer)
+    rate = sqla.Column(sqla.Float)
+
+    def __init__(self, name, followers, posts, rate):
+        self.name = name
+        self.followers = followers
+        self.posts = posts
+        self.rate = rate
+
+    def to_json(self):
+        return {
+            "account": self.name,
+            "followers": self.followers,
+            "posts": self.posts,
+            "rate": self.rate
+        }
+
+
+Base.metadata.create_all(engine)
 
 
 @app.route("/")
@@ -26,29 +64,43 @@ def get_stats():
     return str(_get_stats())
 
 
-@cachetools.cached(cache=cachetools.TTLCache(maxsize=16, ttl=300))
 def _get_stats():
-    agent = agents.Agent()
     stats = []
     for acc_id in json.load(open("accounts.json"))["accounts"]:
-        account = entities.Account(acc_id)
         try:
-            data, _ = agent.get_media(account)
+            data = _get_user_data(acc_id)
         except Exception as e:
-            log.error("Failed to get data for account %s", acc_id)
+            log.error("Failed to get data for account: %s\n%s", acc_id, e)
             continue
-        rate = 0
-        posts = min(len(data), 12)
-        for i in range(posts):
-            rate += data[i].likes_count + data[i].comments_count
-        stats.append({
-            "account": acc_id,
-            "followers": account.followers_count,
-            "posts": account.media_count,
-            "rate": rate * 100 / float(posts) / float(account.followers_count)
-        })
+        stats.append(data)
+
     sorted_stats = sorted(stats, key=lambda x: x["rate"], reverse=True)
     return sorted_stats
+
+
+def _get_user_data(account_name):
+    session = Session()
+    for user in session.query(User).filter(User.name==account_name):
+        return user.to_json()
+    account = entities.Account(account_name)
+    data, _ = agent.get_media(account)
+    rate = 0
+    posts = min(len(data), 12)
+    for i in range(posts):
+        rate += data[i].likes_count + data[i].comments_count
+    user = User(
+        name=account_name,
+        followers=account.followers_count,
+        posts=account.media_count,
+        rate=rate * 100 / float(posts) / float(account.followers_count))
+    session.add(user)
+    session.commit()
+    return {
+        "account": account_name,
+        "followers": account.followers_count,
+        "posts": account.media_count,
+        "rate": rate * 100 / float(posts) / float(account.followers_count)
+    }
 
 
 if __name__ == '__main__':
