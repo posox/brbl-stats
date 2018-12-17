55bbe529bec4b0265f516ea8095bf4a4e4989885
Sergey Reshetnyak <sreshetniak@mirantis.com>
2018-12-17 22:46:31 +0400

    Simple stats visualisation

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@
 
 import json
 
+import flask
 from flask import Flask
 from instaparser import agents
 from instaparser import entities
@@ -10,22 +11,34 @@ from instaparser import entities
 app = Flask(__name__)
 
 
+@app.route("/")
+@app.route("/index")
+def index():
+    data = _get_stats()
+    return flask.render_template("index.html", data=data)
+
+
 @app.route("/stats")
 def get_stats():
+    return str(_get_stats())
+
+
+def _get_stats():
     agent = agents.Agent()
-    stats = {}
+    stats = []
     for acc_id in json.load(open("accounts.json"))["accounts"]:
         account = entities.Account(acc_id)
         data, _ = agent.get_media(account)
         likes = []
         for i in range(min(len(data), 10)):
             likes.append(data[i].likes_count)
-        stats[acc_id] = {
+        stats.append({
+            "account": acc_id,
             "followers": account.followers_count,
             "posts": account.media_count,
             "last_likes_count": likes
-        }
-    return str(stats)
+        })
+    return stats
 
 
 if __name__ == '__main__':
